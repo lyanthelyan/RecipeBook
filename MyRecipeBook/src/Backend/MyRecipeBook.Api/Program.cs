d15680b610525c22552322dffbd73558d2a4e84c
lyanthelyan <emyo.elyan@gmail.com>
2026-05-28 15:32:27 -0300

    ExceptionFilter created

--- MyRecipeBook/src/Backend/MyRecipeBook.Api/Program.cs
+++ MyRecipeBook/src/Backend/MyRecipeBook.Api/Program.cs
@@ -9,19 +9,26 @@ builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(); // Swagger Configuration
 builder.Services.AddOpenApi();
 
-builder.Services.Configure<RequestLocalizationOptions>(options => 
+builder.Services.Configure<RequestLocalizationOptions>(options =>
 {
-    var supportedCultures = new List<CultureInfo> 
-    { 
+    // Supported languages
+    var supportedCultures = new List<CultureInfo>
+    {
         new CultureInfo("en"),
         new CultureInfo("pt-BR"),
         new CultureInfo("es")
     };
 
+    // Default language
     options.DefaultRequestCulture = new RequestCulture("en");
+
+    // Formatting cultures (dates, numbers, currency)
     options.SupportedCultures = supportedCultures;
+
+    // UI/message cultures
     options.SupportedUICultures = supportedCultures;
 
+    // Gets user language from browser
     options.RequestCultureProviders = new List<IRequestCultureProvider>
     {
         new AcceptLanguageHeaderRequestCultureProvider()
--- MyRecipeBook/src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountUseCase.cs
+++ MyRecipeBook/src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountUseCase.cs
@@ -7,12 +7,22 @@ public class RegisterUserAccountUseCase
 {
     public void Execute(RequestRegisterUserAccountJson request)
     {
+        // Creates the validator responsible
+        // for validating the request data
         var validator = new RegisterUserAccountValidator();
+
+        // Executes the validation
         var result = validator.Validate(request);
 
+        // Checks if the validation failed
         if (result.IsValid == false)
         {
-            var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+            // Gets all validation error messages
+            var errorMessages = result.Errors
+                .Select(error => error.ErrorMessage)
+                .ToList();
+
+            // Throws a custom validation exception
             throw new ErrorOnValidationException(errorMessages);
         }
     }
