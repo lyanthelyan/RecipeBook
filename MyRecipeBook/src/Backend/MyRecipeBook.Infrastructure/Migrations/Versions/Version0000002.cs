fc6f9e60fae66cd01ff57cefe3aa9875722be19c
lyanthelyan <emyo.elyan@gmail.com>
2026-08-04 17:18:10 -0300

    Migrations for recipes added

---
+++ MyRecipeBook/src/Backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version0000002.cs
@@ -0,0 +1,45 @@
+using FluentMigrator;
+using System.Data;
+
+namespace MyRecipeBook.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersions.TABLE_RECIPES, "Creating Recipes table")]
+public class Version0000002 : Migration
+{
+    public override void Up()
+    {
+        Create.Table("Recipes")
+            .WithColumn("Id").AsGuid().NotNullable().PrimaryKey()
+            .WithColumn("Active").AsBoolean().NotNullable().WithDefaultValue(true)
+            .WithColumn("Title").AsString(250).NotNullable()
+            .WithColumn("CookTime").AsInt32().NotNullable()
+            .WithColumn("UserId").AsGuid().NotNullable().ForeignKey("Users", "Id");
+
+        Create.Table("RecipeIngredients")
+            .WithColumn("Id").AsGuid().NotNullable().PrimaryKey()
+            .WithColumn("Active").AsBoolean().NotNullable().WithDefaultValue(true)
+            .WithColumn("Item").AsString(250).NotNullable()
+            .WithColumn("RecipeId").AsGuid().NotNullable().ForeignKey("Recipes", "Id").OnDelete(Rule.Cascade);
+
+        Create.Table("RecipeInstructions")
+            .WithColumn("Id").AsGuid().NotNullable().PrimaryKey()
+            .WithColumn("Active").AsBoolean().NotNullable().WithDefaultValue(true)
+            .WithColumn("Order").AsInt32().NotNullable()
+            .WithColumn("Description").AsString(2000).NotNullable()
+            .WithColumn("RecipeId").AsGuid().NotNullable().ForeignKey("Recipes", "Id").OnDelete(Rule.Cascade);
+
+        Create.Table("RecipeDishTypes")
+            .WithColumn("Id").AsGuid().NotNullable().PrimaryKey()
+            .WithColumn("Active").AsBoolean().NotNullable().WithDefaultValue(true)
+            .WithColumn("Types").AsInt32().NotNullable()
+            .WithColumn("RecipeId").AsGuid().NotNullable().ForeignKey("Recipes", "Id").OnDelete(Rule.Cascade);
+    }
+
+    public override void Down()
+    {
+        Delete.Table("RecipeDishTypes");
+        Delete.Table("RecipeInstructions");
+        Delete.Table("RecipeIngredients");
+        Delete.Table("Recipes");
+    }
+}
