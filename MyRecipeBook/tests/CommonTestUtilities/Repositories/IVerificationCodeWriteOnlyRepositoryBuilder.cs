5f29877245a30aaf8efa58590905e37cf6308cbe
lyanthelyan <emyo.elyan@gmail.com>
2026-08-14 14:44:23 -0300

    Tests for RequestPasswordRecovery added

---
+++ MyRecipeBook/tests/CommonTestUtilities/Repositories/IVerificationCodeWriteOnlyRepositoryBuilder.cs
@@ -0,0 +1,13 @@
+﻿namespace CommonTestUtilities.Repositories;
+
+using Moq;
+using MyRecipeBook.Domain.Repositories.VerificationCode;
+
+public class IVerificationCodeWriteOnlyRepositoryBuilder
+{
+    public static IVerificationCodeWriteOnlyRepository Build()
+    {
+        var mock = new Mock<IVerificationCodeWriteOnlyRepository>();
+        return mock.Object;
+    }
+}
---
+++ MyRecipeBook/tests/CommonTestUtilities/Requests/RequestPasswordRecoveryJsonBuilder.cs
@@ -0,0 +1,12 @@
+﻿using MyRecipeBook.Communication.Requests;
+using Bogus;
+namespace CommonTestUtilities.Requests;
+
+public class RequestPasswordRecoveryJsonBuilder
+{
+    public static RequestPasswordRecoveryJson Build()
+    {
+        return new Faker<RequestPasswordRecoveryJson>()
+            .RuleFor(request => request.Email, f => f.Internet.Email());
+    }
+}
